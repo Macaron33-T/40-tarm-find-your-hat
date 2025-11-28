3b5d9cddf4d930d2fa1ad0f4289a80646d302321
Macaron33-T <tarm3521@gmail.com>
2025-11-28 15:27:38 +0700

    Add  Movement

--- main.js
+++ main.js
@@ -10,11 +10,13 @@ const HOLE = "O";
 const HAT = "^";
 
 // Hardcoded board
-let board = [
-	[PLAYER, EMPTY, HOLE],
-	[EMPTY, HOLE, EMPTY],
-	[EMPTY, HAT, EMPTY],
-];
+// let board = [
+//   [PLAYER, EMPTY, HOLE],
+//   [EMPTY, HOLE, EMPTY],
+//   [EMPTY, HAT, EMPTY],
+// ];
+
+const Sboard = board.map((row) => row.join("")).join("\n");
 
 // Game state
 let playerRow = 0;
@@ -22,12 +24,46 @@ let playerCol = 0;
 let playing = true;
 
 // Print board
-function printBoard(board) {
-	console.clear(); // call console.clear() before print each move
-	console.log(board);
+// function printBoard(Sboard) {
+//   console.clear(); // call console.clear() before print each move
+//   console.log(Sboard);
+// }
+
+// printBoard(Sboard);
+// const input = prompt("Which way? (w/a/s/d): ");
+// console.log(input);
+
+//control
+let row = 0;
+let col = 0;
+moves.push(board[row][col]);
+
+function playerMovem(input) {
+  let newRow = row;
+  let newCol = col;
 }
 
-// Game play loop
-printBoard(board);
-const input = prompt("Which way? (w/a/s/d): ");
-console.log(input);
+switch (input.toLowerCase()) {
+  case "w":
+    newRow--;
+    break;
+  case "s":
+    newRow++;
+    break;
+  case "a":
+    newCol--;
+    break;
+  case "d":
+    newCol++;
+    break;
+  default:
+    console.log("ONLY W A S D ");
+    return;
+}
+
+printField();
+
+while (true) {
+  const input = prompt("ONLY W A S D");
+  playerMovem(input);
+}
